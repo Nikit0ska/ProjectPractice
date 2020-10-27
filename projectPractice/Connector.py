0b99f4262b1ae0648a929752be135ebfda07fc39
Nikit0ska <nik.ryabov.2014@mail.ru>
2020-10-27 17:38:08 +0300

    Теперь коннектор в виде класса

---
+++ projectPractice/Connector.py
@@ -0,0 +1,19 @@
+import pyodbc
+
+# Для MongoDB: https://www.cdata.com/drivers/mongodb/download/odbc/
+# Для PosgreSQL: https://ftp.postgresql.org/pub/odbc/versions/msi/psqlodbc_12_02_0000.zip
+# Посмотреть установленные драйвера: pyodbc.drivers()
+
+class Connector:
+    def __init__(self, driver, server, port, uid=str(), pwd=str(), database=str(), autocommit=True):
+
+        self.connection = pyodbc.connect(f'DRIVER={{{driver}}};SERVER={server};PORT={port};DATABASE={database};UID={uid}'
+                                             f';PWD={pwd};')
+
+        self.cursor = self.connection.cursor()
+        self.connection.autocommit = autocommit
+
+    def execute(self, query):
+        self.cursor = self.connection.execute(query)
+        return self.cursor
+
