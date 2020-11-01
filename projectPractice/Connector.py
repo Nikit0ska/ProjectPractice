c896410892c04889c547e0da1b4408f204ba33e1
Nikita <nik.ryabov.2014@mail.ru>
2020-11-01 21:16:51 +0300

    Коннектор в виде класса

--- projectPractice/Connector.py
+++ projectPractice/Connector.py
@@ -6,6 +6,7 @@ import time
 # Для MongoDB: https://www.cdata.com/drivers/mongodb/download/odbc/
 # Для PosgreSQL: https://ftp.postgresql.org/pub/odbc/versions/msi/psqlodbc_12_02_0000.zip
 # Посмотреть установленные драйвера: pyodbc.drivers()
+#
 
 class Connector:
     def __init__(self, driver, server, port, database, uid=str(), pwd=str(), autocommit=True):
