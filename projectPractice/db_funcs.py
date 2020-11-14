027150baf2332cb4f1d6061f17d1c14140f6aa2b
nikit0ska <nik.ryabov.2014@mail.ru>
2020-11-14 21:19:50 +0300

    Ловля исключений

--- projectPractice/db_funcs.py
+++ projectPractice/db_funcs.py
@@ -2,51 +2,45 @@ import threading
 
 from projectPractice.Connector import Connector
 from projectPractice.ConnThread import ConnThread
+import projectPractice.errors as errors
 
 
 # Для MongoDB: https://www.cdata.com/drivers/mongodb/download/odbc/
 # Для PosgreSQL: https://ftp.postgresql.org/pub/odbc/versions/msi/psqlodbc_12_02_0000.zip
 # Посмотреть установленные драйвера: pyodbc.drivers()
 
-def db_connect(driver, server, port, db, user='', password=''):
-    conn = Connector(driver, server, port, db, uid=user, pwd=password)
+
+def db_connect(driver, server, port, db, user='', password='', autocomm=True):
+    conn = Connector(driver, server, port, db, uid=user, pwd=password, autocommit=autocomm)
     thread = ConnThread(conn)
     thread.start()
 
 
 def __get_conn_thread():
+    con_thread = None
     for thread in threading.enumerate():
         if type(thread) == ConnThread:
-            return thread
-    return None
+            con_thread = thread
+            return con_thread
+    if con_thread is None:
+        raise errors.OdbcConnectionError('No connection')
 
 
+@errors.err_decor
 def db_disconnect():
     conn_thread = __get_conn_thread()
     conn_thread.stop_thread()
-    return 'disconnection is successful'
+    return None
 
 
+@errors.err_decor
 def db_read_table(table_name):
     conn = __get_conn_thread().connector
     conn.execute(f"SELECT * FROM {table_name};")
     return conn.cursor.fetchall()
 
 
+@errors.err_decor
 def db_execute_query(query):
-    if __get_conn_thread() is not None:
-        conn = __get_conn_thread().connector
-        conn.execute(query)
-    else:
-        return "Can't find connection"
-
-
-# db_connect('PostgreSQL ANSI', 'localhost', 5432, 'postgres', user='postgres', password='abc123')
-# print(threading.enumerate())
-# db_disconnect()
-# print(threading.enumerate())
-# db_connect('PostgreSQL ANSI', 'localhost', 5432, 'postgres', user='postgres', password='abc123')
-# print(threading.enumerate())
-# query_execute("INSERT INTO test VALUES(3, 'test');")
-# print(db_read_table('test'))
-# db_disconnect()
+    conn = __get_conn_thread().connector
+    conn.execute(query)
