5e5ff89183c275a15e2951d4785aade330272c5d
nikit0ska <nik.ryabov.2014@mail.ru>
2020-11-14 21:20:55 +0300

    Поднятие своего исключения

--- projectPractice/ConnThread.py
+++ projectPractice/ConnThread.py
@@ -1,11 +1,12 @@
 import threading
+from projectPractice.errors import OdbcConnectionError
 
 
 class ConnThread(threading.Thread):
     def __init__(self, connector):
         for thread in threading.enumerate():
             if type(thread) == ConnThread:
-                raise ConnectionError
+                raise OdbcConnectionError('Connection already exist')
         super().__init__(target=self.__inf, daemon=True)
         self.connector = connector
 
