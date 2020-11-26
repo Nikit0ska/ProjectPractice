291011f7ba713d3f952f4c6dc9ba5aca635910b5
nikit0ska <nik.ryabov2014@Mail.ru>
2020-11-26 03:28:30 +0300

    main.py

--- projectPractice/main.py
+++ projectPractice/main.py
@@ -1,9 +1,18 @@
 from projectPractice import db_funcs
+from projectPractice.Table import Table
 import pyodbc
 
 
 print(pyodbc.drivers())
 
 db_funcs.db_connect('PostgreSQL Unicode', 'localhost', 5432, 'nikit0ska', user='nikit0ska', password='abc123', autocomm=True)
-a = db_funcs.db_execute_query('SELECT * FROM test')
-print(len(a))
+table = Table('test')
+# print(table.columns)
+
+# table.id()
+# table.string('username', 100)
+# table.integer('age')
+# query = table.create_table()
+# db_funcs.db_execute_query(query)
+
+
