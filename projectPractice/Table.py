dd7d82cb37d9f394f2df501b5193d923c3133816
nikit0ska <nik.ryabov.2014@mail.ru>
2020-12-03 21:20:59 +0300

    foreign_key

--- projectPractice/Table.py
+++ projectPractice/Table.py
@@ -23,6 +23,36 @@ class Field:
         return self
 
 
+class ForeignField:
+    def __init__(self, name, type):
+        self._field_name = name
+        self._field_type = type
+        self._params = {"constraint": '', "foreign_key": f"FOREIGN KEY({self._field_name})",
+                        "references": '', 'on_delete': ''}
+
+    def constraint(self, constraint):
+        self._params["constraint"] = f"CONSTRAINT {constraint}"
+        return self
+
+    def references(self, table, field):
+        self._params["references"] = f"REFERENCES {table}({field})"
+        return self
+
+    def on_delete(self, set_null=False, set_default=False, restrict=False, cascade=False):
+        if set_null:
+            self._params["on_delete"] = f"ON DELETE SET NULL"
+            return self
+        elif set_default:
+            self._params["on_delete"] = f"ON DELETE SET DEFAULT"
+            return self
+        elif restrict:
+            self._params["on_delete"] = f"ON DELETE RESTRICT"
+            return self
+        elif cascade:
+            self._params["on_delete"] = f"ON DELETE CASCADE"
+            return self
+
+
 class CreatedTable:
     def __init__(self, table_name):
         self.table = table_name
@@ -109,32 +139,36 @@ class NewTable:
         field._params.append('UNIQUE')
         self.fields.append(field)
 
-    def integer(self, name):
+    def integer(self, name, is_foreign=False):
+        if is_foreign:
+            field = ForeignField(name, 'integer')
+            self.fields.append(field)
+            return field
         field = Field(name, 'integer')
         self.fields.append(field)
         return field
 
-    def text(self, name):
+    def text(self, name, is_foreign=False):
         field = Field(name, 'text')
         self.fields.append(field)
         return field
 
-    def string(self, name, length):
+    def string(self, name, length, is_foreign=False):
         field = Field(name, f'varchar({length})')
         self.fields.append(field)
         return field
 
-    def bool(self, name):
+    def bool(self, name, is_foreign=False):
         field = Field(name, 'boolean')
         self.fields.append(field)
         return field
 
-    def binary(self, name):
+    def binary(self, name, is_foreign=False):
         field = Field(name, 'bytea')
         self.fields.append(field)
         return field
 
-    def char(self, name):
+    def char(self, name, is_foreign=False):
         field = Field(name, 'char')
         self.fields.append(field)
         return field
@@ -143,14 +177,23 @@ class NewTable:
         sql = f"CREATE TABLE {self.table}("
         for elem in self.fields:
             tmp = f"{elem._field_name} {elem._field_type}"
-            for param in elem._params:
-                tmp += f" {param}"
+            if type(elem) != ForeignField:
+                for param in elem._params:
+                    tmp += f" {param}"
             tmp += ", "
             sql += tmp
+            tmp = ''
+        for elem in self.fields:
+            if type(elem) == ForeignField:
+                for param in elem._params:
+                    tmp += f" {elem._params[param]}"
+                tmp += ", "
+                sql += tmp
         sql = sql.rstrip(', ')
         sql += ");"
-        db_funcs.db_execute_query(sql)
-        return Table(self.table)
+        print(sql)
+        # db_funcs.db_execute_query(sql)
+        # return Table(self.table)
 
 
 class Table:
