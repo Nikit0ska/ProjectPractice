d43ec61bf2f6d358d67a58396f9094adb40b52ae
nikit0ska <nik.ryabov.2014@mail.ru>
2020-12-09 15:07:44 +0300

    main.py

--- projectPractice/main.py
+++ projectPractice/main.py
@@ -6,8 +6,7 @@ import pyodbc
 print(pyodbc.drivers())
 
 db_funcs.db_connect('PostgreSQL Unicode', 'localhost', 5432, 'nikit0ska', user='nikit0ska', password='abc123', autocomm=True)
-b = Table('test1')
 
-b.integer('foreign_field', is_foreign=True).constraint('some_cons').on_delete(set_null=True).references('some_table', 'some_field')
 
-b.create_table()
+
+
