572b3460801ffe81a4390403709b1847962fa9e1
nikit0ska <nik.ryabov.2014@mail.ru>
2020-12-24 15:07:07 +0300

    main.py

--- projectPractice/main.py
+++ projectPractice/main.py
@@ -6,7 +6,6 @@ import pyodbc
 print(pyodbc.drivers())
 
 db_funcs.db_connect('PostgreSQL Unicode', 'localhost', 5432, 'nikit0ska', user='nikit0ska', password='abc123', autocomm=True)
-
-
-
+# db_funcs.import_csv_db('csv_table','cities.csv')
+# db_funcs.import_xl_db('test', 'Financial Sample.xlsx')
 
